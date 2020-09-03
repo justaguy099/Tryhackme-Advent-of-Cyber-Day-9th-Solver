b1bc6623ce478127f4d6dfbc9067cb714307b366
justaguy099 <48881628+justaguy099@users.noreply.github.com>
2020-09-03 01:56:05 +0000

    Update request.py

--- request.py
+++ request.py
@@ -1,12 +1,12 @@
 import requests
 
 path = 'f'
-host = 'http://10.10.169.100:3000/'
+host = 'http://10.10.169.100:3000/' #This is the path for the host
 
 while(path != "end"):
 	response = requests.get(host+path)
 	print(response.json(), end = ' ')
-	path = input("whats next?:")	
+	path = input("whats next?:") #for this input you need to supplement it with the value of 'next' of the json response
 	
 
 	
